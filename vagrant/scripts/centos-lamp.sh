06304b533ac5c1b433d959e2e2b1ab9691c6d0b8
Nishanthk3 <nishanth.k3@gmail.com>
2016-02-11 20:20:51 -0600

    setting LAMP, added files

---
+++ vagrant/scripts/centos-lamp.sh
@@ -0,0 +1,37 @@
+#!bin/bash
+
+# Update CentOS with nay patches
+yum update -y --exclude=kernel
+
+# Tools
+yum install -y nano git unzip screen
+
+#Apache
+yum install -y hhtpd httpd-devel httpd-tools
+chkconfig --add http
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+
+#MySQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysql on
+
+service mysql start
+
+mysql -u root -e "SHOW DATABASES";
+
+
+# Download starter content
+
+service httpd restart
+
