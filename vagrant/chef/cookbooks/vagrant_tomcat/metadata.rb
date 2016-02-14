ffabdafd8038f0c0076e5641293fbe323b4bdf11
Nishanthk3 <nishanth.k3@gmail.com>
2016-02-14 14:28:29 -0600

    adding metadata file for new custom cookbook

---
+++ vagrant/chef/cookbooks/vagrant_tomcat/metadata.rb
@@ -0,0 +1,14 @@
+name 'vagrant_tomcat'
+maintainer 'Nishanth Reddy Kommidi'
+maintainer_email 'nishanth.k3@gmail.com'
+license 'Apache 2.0'
+description 'custom tomcat_latest cookbook for vagrant'
+long_desciption IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.1'
+
+depends 'java'
+depends 'tomcat_latest'
+
+supports 'centos'
+
+recipe 'vagrant_tomcat::Default', 'Updates Tomcat installation'
