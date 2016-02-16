a8709e0b9c88ab60c47d4be5ce261e4480681408
Nishanthk3 <nishanth.k3@gmail.com>
2016-02-15 21:18:33 -0600

    committing syntax errors

---
+++ vagrant/chef/cookbooks/vagrant_tomcat/README.md
@@ -0,0 +1,4 @@
+#Vagrant Tomcat
+#usage
+
+Include this cookbook, using the deafult recipe **vagrant_tomcat::default**
--- vagrant/chef/cookbooks/vagrant_tomcat/recipes/default.rb
+++ vagrant/chef/cookbooks/vagrant_tomcat/recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe "tomcat_latest"
 
-file "/etd/rc.d/tomcat" do
+file "/etc/rc.d/tomcat" do
 	action "delete"
 end
 
